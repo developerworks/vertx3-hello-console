b60ab4b3d1219873119c8006f59a2cc2aa993a94
developerworks <developerworks@163.com>
2017-10-03 23:29:45 +0800

    Add workable eventbus demo

--- src/bridge.js
+++ src/bridge.js
@@ -1,21 +1,36 @@
 var Router = require("vertx-web-js/router");
 var SockJSHandler = require("vertx-web-js/sock_js_handler");
+var StaticHandler = require("vertx-web-js/static_handler");
 
 var router = Router.router(vertx);
 var options = {
-  "outboundPermitteds": [{ "address": "my_message_address" }],
-  "inboundPermitteds": [{ "address": "my_message_address" }],
-  "pingTimeout": 5000
+  "outboundPermitteds": [{ "address": "clients" }],
+  "inboundPermitteds": [{ "address": "server" }],
+  "pingTimeout": 3000,     // PING超时, 如果60秒内服务器没有收到客户的PING消息, 则认为客户端断开连接.
+  "heartbeatInterval": 1000 // 客户端向服务器发送PING消息的间隔
 };
 var sockJSHandler = SockJSHandler.create(vertx).bridge(options, function (be) {
-  if (be.type() !== 'SOCKET_CREATED' && be.type() !== 'SOCKET_CLOSED') {
-    console.log(be.getRawMessage().body)
-  } else {
-    console.log(be.type)
+  if (be.type() === 'SOCKET_PING') {
+
   }
-  be.complete(true);
 });
+
 router.route("/eventbus/*").handler(sockJSHandler.handle);
-vertx.createHttpServer().requestHandler(router.accept).listen(8080, function () {
-  console.log('Socket server is listen on ws://localhost:8080')
+router.route("/static/*").handler(StaticHandler.create().handle);
+
+vertx
+  .createHttpServer()
+  .requestHandler(router.accept)
+  .listen(8080, function () {
+    console.log('Socket server is listen on ws://localhost:8080')
+  });
+
+var eb = vertx.eventBus();
+eb.consumer("server").handler(function (message) {
+  console.log(message.body())
+  eb.publish("clients", message.body());
 });
+// eb.consumer("ping-server").handler(function (message) {
+//   console.log(message.body());
+//   eb.publish("ping-client", message.body());
+// })
--- src/sockjs.js
+++ src/sockjs.js
@@ -1,3 +1,12 @@
+// var Router = require("vertx-web-js/router");
+// var SockJSHandler = require("vertx-web-js/sock_js_handler");
+// var router = Router.router(vertx);
+// var options = {
+//   "heartbeatInterval": 2000
+// };
+// var sockJSHandler = SockJSHandler.create(vertx, options);
+// router.route("/ws").handler(sockJSHandler.handle);
+
 var Router = require("vertx-web-js/router");
 var SockJSHandler = require("vertx-web-js/sock_js_handler");
 var router = Router.router(vertx);
@@ -5,12 +14,13 @@ var options = {
   "heartbeatInterval": 2000
 };
 var sockJSHandler = SockJSHandler.create(vertx, options);
-router.route("/ws").handler(sockJSHandler.handle);
-// var options = {
-//   "outboundPermitteds": [{ "address": "draw" }],
-//   "inboundPermitteds": [{ "address": "draw" }]
-// };
-// router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(options).handle);
+sockJSHandler.socketHandler(function (sockJSSocket) {
+  // Just echo the data back
+  sockJSSocket.handler(sockJSSocket.write);
+});
+router.route("/ws/*").handler(sockJSHandler.handle);
+
+
 vertx.createHttpServer().requestHandler(router.accept).listen(8080, function () {
   console.log('Socket server is listen on ws://localhost:8080')
 });
