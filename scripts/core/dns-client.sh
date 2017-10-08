3f62a95ad6149c4909ed228f0cbbd5d340681eb6
developerworks <developerworks@163.com>
2017-10-09 00:52:01 +0800

    remove not needed shell variable

--- scripts/core/dns-client.sh
+++ scripts/core/dns-client.sh
@@ -6,4 +6,4 @@ export CLASSPATH=$CLASSPATH:./src/main/resources
 RUN="./node_modules/.bin/vertx run"
 JAVA_OPT="-Duser.language=en -Duser.country=US -Duser.variant=en_US"
 
-${RUN} --java-opts=${JAVA_OPT} ${LAUNCHER} --launcher-class=io.vertx.core.Launcher --redeploy=./src/core/dns-client/*.js ./src/core/dns-client/index.js
+${RUN} --java-opts=${JAVA_OPT} --launcher-class=io.vertx.core.Launcher --redeploy=./src/core/dns-client/*.js ./src/core/dns-client/index.js
--- scripts/rx/readfile.sh
+++ scripts/rx/readfile.sh
@@ -2,8 +2,9 @@
 
 export CLASSPATH=./src/conf
 export CLASSPATH=$CLASSPATH:./src/main/resources
+export CLASSPATH=$CLASSPATH:./node_modules/rx/dist
 
 RUN="./node_modules/.bin/vertx run"
 JAVA_OPT="-Duser.language=en -Duser.country=US -Duser.variant=en_US"
 
-${RUN} --java-opts=${JAVA_OPT} --launcher-class=io.vertx.core.Launcher --redeploy=./src/core/logging/*.js ./src/core/logging/index.js
+${RUN} --java-opts=${JAVA_OPT} --launcher-class=io.vertx.core.Launcher --redeploy=./src/rx/*.js ./src/rx/readfile.js
